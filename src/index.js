7e3ddad81cc415bab01e315b54ee4bf6a429139b
Nabil <33580@ma-web.nl>
2024-01-20 20:01:03 +0100

    remove border bottom

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
